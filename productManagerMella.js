e55c9391ea89aa2210214c8586225605ad946fdd
Hector Javier <hectorjaviermella@gmail.com>
2023-02-28 20:30:27 -0300

    Add files via upload

--- productManagerMella.js
+++ productManagerMella.js
@@ -1,79 +1,120 @@
-class ProductManager{
-    
+import fs from "fs";
 
-    constructor(){
-        this.products = [];
-    
-    }
+export default class ProductManager{
 
-    ////////////////////////////////////////////////////////////////////////////////    
-
-  getProducts = () => {
-    console.log(this.products);
-    return this.products;    //devuelve array de productos
-    
-}    
-
-/////////////////////////////////////////////////////////////////////// 
-  addProduct = (pTitle,pDescription,pPrice,pThumbnail,pCode,pStock) => {
-
-    if (!pTitle  || !pDescription || !pPrice || !pThumbnail || !pCode || !pStock){
-        console.log("error  es obligatorio")
-        return;
-    }
+         constructor(){
+           
+            this.path = "./files/Productos.json";
+         }
+////////////////////////////////////////////////////////////////////
+   getProducts = async () => {    //devuelve array de productos
+         if (fs.existsSync(this.path)){
+                const data = await fs.promises.readFile(this.path, "utf-8");
+                const result = JSON.parse(data);
+                //console.log(result);
+                return result;
+             } else {
+                return [];
+             }
+          };
 
 
-    const productIndex = this.products.findIndex((product) => product.pCode === pCode);
 
-      if (productIndex === -1)  {   //no encuentra  el producto
-          const product ={
-             pId: this.products.length +1,
-             pCode: pCode,
-             pDescription: pDescription,
-             pPrice:pPrice,
-             pThumbnail:pThumbnail,
-             pStock:pStock,
-             
-        };
 
-        this.products.push(product);
+ ////////////////////////////////////////////////////////////////////
+   addProduct = async(producto) => {
+            console.log("entro al crear producto");
+            const productos = await this.getProducts();
+            if (productos.length === 0){               
+                producto.pId = 1;
+            }else {
+               producto.pId = productos[productos.length - 1].pId +1;
+            }
 
-        console.log(`Product with code ${pCode} add succesfull`)
+            productos.push(producto);            
+            await fs.promises.writeFile(this.path, JSON.stringify(productos,null,"\t"));
+            return producto;
+          };
 
-      }else{
-        console.log(`Product with code ${pCode} already exists`)
-         
-      }
 
+///////////////// ////////////////////////////////////////////////////////////////////
+getProductsById= async(pId) => {
+
+         if (fs.existsSync(this.path)){
+           
+           const productos = await this.getProducts();
+            const indiceproducto = productos.findIndex(producto => producto.pId === pId);
+                        
+            
+            if (indiceproducto === -1){ //no encontro el producto
+               console.log(`Producto con  id ${pId} no existe`)
+                return [];
+            }else{ //encontro el  producto 
+                console.log(`Producto con id ${pId}  existe `)
+                return(productos[indiceproducto]);
+            }           
+         } else {   //archivo no existe
+            return [];
+         }
 };
+ ////////////////////////////////////////////////////////////////////
+ //  eliminarProducto
+ deleteProduct = async (pId) => {   
+   if (fs.existsSync(this.path)){
+          console.log("entro a eliminar un producto");
+          const productos = await this.getProducts();
+          const indiceproducto = productos.findIndex(producto => producto.pId === pId);          
 
-////////////////////////////////////////////
- getProductsById = (pId) =>{
-    const productsearch = this.products.findIndex((product) => product.pId === pId)
+          if (indiceproducto === -1){ //no encontro el producto
+            console.log(`El producto con id  ${pId} no existe`)
+             return [];
+         }else{ //encontro el  producto 
+       
+            for( var i = 0; i < productos.length; i++){                                    
+               if ( productos[i].pId === pId) { 
+                  productos.splice(i, 1); 
+                   i--; 
+               }
+           }
+            await fs.promises.writeFile(this.path, JSON.stringify(productos,null,"\t"));            
+            return productos;
+         }               
+      
+                           };
+};
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+ //update producto
 
-    if (productsearch === -1){
-       console.log(`Product with id ${pId} was not found`)
-    }else{
-        console.log(`Product with id ${pId}  exist `)
-        console.log(this.products[productsearch]);
+    updateProducto= async (productoudpate) => {
+         if (fs.existsSync(this.path)){
+            console.log("entro a modificar un producto");
+            const productos = await this.getProducts();
+            const indiceproducto = productos.findIndex(producto => producto.pId === productoudpate.pId); 
 
-    }
+            if (indiceproducto === -1){ //no encontro el producto
+            console.log(`El Producto con id ${productoudpate.pId} no existe`)
+               return [];
+            }else{ //encontro el  producto 
+               //preparar objeto a modificar
+               
+               productoudpate.pid ??= productos[indiceproducto].pid;
+               productoudpate.pCode ??= productos[indiceproducto].pCode;
+               productoudpate.pTitle ??= productos[indiceproducto].pTitle;
+               productoudpate.pDescription ??= productos[indiceproducto].pDescription;
+               productoudpate.pPrice ??= productos[indiceproducto].pPrice;
+               productoudpate.pThumbnail ??= productos[indiceproducto].pThumbnail;
+               productoudpate.pStock ??= productos[indiceproducto].pStock;
 
- };
+               productos[indiceproducto] = productoudpate;
+               
+               await fs.promises.writeFile(this.path, JSON.stringify(productos,null,"\t"));
+               console.log(productos);
+               return productos;
+            }   
+          
 
-
+               };
+};
 
 
 }
-
-//////////////////////////////////////////////
-
-let products = new ProductManager();
-products.getProducts();
-
-
-products.addProduct("monitor", "monitor 23 pulgadas" , 23000, "sin imagen","abc123", 23);
-products.getProducts();
-products.addProduct("monitor", "monitor 23 pulgadas" , 23000, "sin imagen","abc123", 23);
-
-products.getProductsById(1);
